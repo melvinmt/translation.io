4254715c712011e4936bb13d74c4108f9a86189b
Melvin Tercan <mmtercan@gmail.com>
2013-03-10 04:10:32 -0700

    adding comments

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 )
 
+// All Resources need to implement this interface
 type Resource interface {
 	Get(*url.Values) (int, APIResponse)
 	Post(*url.Values) (int, APIResponse)
@@ -14,7 +15,8 @@ type Resource interface {
 	Delete(*url.Values) (int, APIResponse)
 }
 
-func MatchRoute(r string, p string) (bool, []string) {
+// matchRoute is a helper method for the Router to interprete paths and to parse optional params
+func matchRoute(r string, p string) (bool, []string) {
 	regex, err := regexp.Compile(r)
 	if err != nil {
 		fmt.Println(err)
@@ -29,50 +31,56 @@ func MatchRoute(r string, p string) (bool, []string) {
 	return false, nil
 }
 
+// The Router method routes requests to the appropriate Resource
 func Router(path string) Resource {
-	if ok, vars := MatchRoute("/strings/?([a-zA-Z0-9]{0,12})?", path); ok {
-		if len(vars[0]) <= 1 {
+	if ok, params := matchRoute("/strings/?([a-zA-Z0-9]{0,12})?", path); ok {
+		if len(params[0]) <= 1 {
 			return &Strings{}
 		}
 		return &Strings{
-			Id: vars[1],
+			Id: params[1],
 		}
 	}
 	return &NotFound{}
 }
 
+// Handler parses all HTTP requests and retrieves responses
 func Handler(w http.ResponseWriter, req *http.Request) {
 
-	var resp APIResponse
+	// Cast APIResponse interface
+	var response APIResponse
 
-	statusCode := 405 // Method not allowed
-	resp = &APIError{
+	// Parse Request
+	req.ParseForm()
+	values := &req.Form
+	path := req.URL.Path
+
+	// Default error when method is not found
+	statusCode := 405
+	response = &APIError{
 		Type:    "invalid-method",
 		Message: "Sorry, this method is not allowed.",
 		Code:    405,
 		Param:   []string{},
 	}
 
-	req.ParseForm()
-	values := &req.Form
-
-	path := req.URL.Path
-
+	// Retrieve response on allowed methods
 	switch req.Method {
 	case "GET":
-		statusCode, resp = Router(path).Get(values)
-		// case "POST":
-		// 	statusCode, body = Router(p).Post(req)
-		// case "PUT":
-		// 	statusCode, body = Router(p).Put(req)
-		// case "DELETE":
-		// 	statusCode, body = Router(p).Delete(req)
+		statusCode, response = Router(path).Get(values)
+	case "POST":
+		statusCode, response = Router(path).Post(values)
+	case "PUT":
+		statusCode, response = Router(path).Put(values)
+	case "DELETE":
+		statusCode, response = Router(path).Delete(values)
 	}
 
+	// Return response
 	if statusCode != 200 {
-		http.Error(w, resp.ToJSON(), 404)
+		http.Error(w, response.ToJSON(), 404)
 	} else {
-		fmt.Fprintf(w, "%s", resp.ToJSON())
+		fmt.Fprintf(w, "%s", response.ToJSON())
 	}
 }
 
